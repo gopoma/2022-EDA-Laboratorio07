357daa51a50d1d30d349f83f8461caa1a31c077e
Gustavo <gordono@unsa.edu.pe>
2022-07-29 00:55:37 -0500

    Implemented traverse and add values to BTreeGeneric functionality charging Logic in BTreeNodeGeneric

--- src/main/java/com/example/project/BTreeGeneric.java
+++ src/main/java/com/example/project/BTreeGeneric.java
@@ -1,10 +1,46 @@
 package com.example.project;
 
 public class BTreeGeneric<E extends Comparable<? super E>> {
+    BTreeNodeGeneric<E> root;
+    int minDeg;
 
-    public boolean add(E value) {
-        //TODO implement here!
-        return false;
+    // Constructor
+    public BTreeGeneric(int minDeg){
+        this.root = null;
+        this.minDeg = minDeg;
+    }
+
+    public void traverse(){
+        if (this.root != null){
+            this.root.traverse();
+        }
+    }
+
+    public void add(E value) {
+        if (this.root == null){
+            this.root = new BTreeNodeGeneric<E>(this.minDeg,true);
+            this.root.keys.set(0, value);
+            this.root.num = 1;
+        }
+        else {
+            // When the root node is full, the tree will grow high
+            if(this.root.num == 2*this.minDeg-1){
+                BTreeNodeGeneric<E> s = new BTreeNodeGeneric<>(this.minDeg,false);
+                // The old root node becomes a child of the new root node
+                s.children.set(0, this.root);
+                // Separate the old root node and give a key to the new node
+                s.splitChild(0, this.root);
+                // The new root node has 2 child nodes. Move the old one over there
+                int i = 0;
+                if(s.keys.get(0).compareTo(value) < 0)
+                    i++;
+                s.children.get(i).insertNotFull(value);
+
+                this.root = s;
+            }
+            else
+                this.root.insertNotFull(value);
+        }
     }
 
     public E remove(E value) {
@@ -24,5 +60,36 @@ public class BTreeGeneric<E extends Comparable<? super E>> {
     public int size() {
         //TODO implement here!
         return 0;
+    }
+
+    public static void main(final String[] args) {
+        BTreeGeneric<Integer> t = new BTreeGeneric<>(2); // A B-Tree with minium degree 2
+        t.add(1);
+        t.add(3);
+        t.add(7);
+        t.add(10);
+        t.add(11);
+        t.add(13);
+        t.add(14);
+        t.add(15);
+        t.add(18);
+        t.add(16);
+        t.add(19);
+        t.add(24);
+        t.add(25);
+        t.add(26);
+        t.add(21);
+        t.add(4);
+        t.add(5);
+        t.add(20);
+        t.add(22);
+        t.add(2);
+        t.add(17);
+        t.add(12);
+        t.add(6);
+
+        System.out.println("Traversal of tree constructed is");
+        t.traverse();
+        System.out.println();
     }
 }
---
+++ src/main/java/com/example/project/BTreeNodeGeneric.java
@@ -0,0 +1,97 @@
+package com.example.project;
+
+import java.util.Vector;
+
+public class BTreeNodeGeneric<E extends Comparable<? super E>> {
+    Vector<E> keys; // keys of nodes
+    int minDeg; // Minimum degree of B-tree node
+    Vector<BTreeNodeGeneric<E>> children;  // Child node
+    int num; // Number of keys of node
+    boolean isLeaf; // True when leaf node
+
+    public static <E> void initializeVectorAsArray(Vector<E> v, int size) {
+        for(int i = 0; i < size; i++)
+            v.add(null);
+    }
+
+    // Constructor
+    public BTreeNodeGeneric(int minDeg, boolean isLeaf) {
+        this.minDeg = minDeg;
+        this.isLeaf = isLeaf;
+        
+        this.keys = new Vector<E>(2*this.minDeg-1); // Node has 2*minDeg-1 keys at most
+        BTreeNodeGeneric.initializeVectorAsArray(this.keys, 2*this.minDeg-1);
+        this.children = new Vector<BTreeNodeGeneric<E>>(2*this.minDeg);
+        BTreeNodeGeneric.initializeVectorAsArray(this.children, 2*this.minDeg);
+        
+        this.num = 0;
+    }
+
+    public void insertNotFull(E key){
+        int i = this.num -1; // Initialize i as the rightmost index
+
+        if(this.isLeaf){ // When it is a leaf node
+            // Find the location where the new key should be inserted
+            while (i >= 0 && this.keys.get(i).compareTo(key) > 0){
+                this.keys.set(i+1, this.keys.get(i)); // keys backward shift
+                i--;
+            }
+            this.keys.set(i+1, key);
+            num = num + 1;
+        }
+        else{
+            // Find the child node location that should be inserted
+            while (i >= 0 && this.keys.get(i).compareTo(key) > 0)
+                i--;
+            if (children.get(i+1).num == 2*this.minDeg - 1){ // When the child node is full
+                splitChild(i+1, this.children.get(i+1));
+                // After splitting, the key in the middle of the child node moves up, and the child node splits into two
+                if (this.keys.get(i+1).compareTo(key) < 0)
+                    i++;
+            }
+            this.children.get(i+1).insertNotFull(key);
+        }
+    }
+
+
+    public void splitChild(int i, BTreeNodeGeneric<E> y){
+
+        // First, create a node to hold the keys of MinDeg-1 of y
+        BTreeNodeGeneric<E> z = new BTreeNodeGeneric<>(y.minDeg, y.isLeaf);
+        z.num = this.minDeg - 1;
+
+        // Pass the properties of y to z
+        for (int j = 0; j < this.minDeg-1; j++)
+            z.keys.set(j, y.keys.get(j+this.minDeg));
+        if (!y.isLeaf){
+            for (int j = 0; j < this.minDeg; j++)
+                z.children.set(j, y.children.get(j+this.minDeg));
+        }
+        y.num = this.minDeg-1;
+
+        // Insert a new child into the child
+        for (int j = num; j >= i+1; j--)
+            this.children.set(j+1, children.get(j));
+        this.children.set(i+1, z);
+
+        // Move a key in y to this node
+        for (int j = num-1;j >= i;j--)
+            this.keys.set(j+1, keys.get(j));
+        this.keys.set(i, y.keys.get(this.minDeg-1));
+
+        num = num + 1;
+    }
+
+    public void traverse(){
+        int i;
+        for (i = 0; i< num; i++){
+            if (!isLeaf)
+                children.get(i).traverse();
+            System.out.printf(" %d", this.keys.get(i));
+        }
+
+        if (!isLeaf){
+            this.children.get(i).traverse();
+        }
+    }
+}
